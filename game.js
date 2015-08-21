00a466d2966ea3b1c77e9456dfd5ecc8adda0edd
Jody Leonard <me@jodyleonard.com>
2015-08-21 13:20:34 -0400

    Basic movement on a canvas.

---
+++ game.js
@@ -0,0 +1,103 @@
+console.log('loaded')
+
+var canvas = document.getElementById('test');
+var context = canvas.getContext('2d');
+var raf;
+
+var square = {
+	x: 10,
+	y: 10,
+	vx: 0,
+	vy: 0,
+	color: 'red',
+	draw: function() {
+		context.fillStyle = this.color;
+		context.fillRect(this.x,this.y,50,50);
+	}
+};
+
+function draw() {
+	//console.log('drawing')
+	context.clearRect(0,0,canvas.width,canvas.height);
+	//context.save();
+	square.draw()
+	square.x += square.vx;
+	square.y += square.vy;
+	raf = window.requestAnimationFrame(draw);
+}
+
+window.addEventListener('keydown',function(e) {
+	//raf = window.requestAnimationFrame(draw);
+	switch (e.which) {
+		case 37:
+			square.vx = -5;
+			console.log('left');
+			break;
+		case 38:
+			square.vy = -5;
+			console.log('up');
+			break;
+		case 39:
+			square.vx = 5;
+			console.log('right');
+			break;
+		case 40:
+			square.vy = 5;
+			console.log('down');
+			break;
+	}
+});
+
+window.addEventListener('keyup',function(e){
+	//window.cancelAnimationFrame(raf);
+	// console.log('keyup')
+	// if (e == 37 || e == 39) {
+	// 	square.vx = 0;
+	// 	console.log('stop horizontal');
+	// }
+	// if (e == 38 || e == 40) {
+	// 	square.vy = 0;
+	// 	console.log('stop vertical');
+	// }
+	horizOff = false;
+	vertOff = false;
+	switch (e.which) {
+		case 37:
+			square.vx = 0;
+			//horizOff = true;
+			console.log('left');
+			break;
+		case 38:
+			square.vy = 0;
+			//vertOff = true;
+			console.log('up');
+			break;
+		case 39:
+			square.vx = 0;
+			//horizOff = true;
+			console.log('right');
+			break;
+		case 40:
+			square.vy = 0;
+			//vertOff = true;
+			console.log('down');
+			break;
+	}
+
+
+	// //doesnt seem to fix much...
+	// if(horizOff) {
+	// 	console.log('horizontal off');
+	// 	square.vx = 0;
+	// }
+	// if(vertOff) {
+	// 	console.log('vertical off');
+	// 	square.vy = 0;
+	// }
+});
+
+function init() {
+	draw();
+}
+
+init();
