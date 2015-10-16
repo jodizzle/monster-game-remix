decf083a75def2f0a55bf9dd8c5f9d564909ada9
Jody Leonard <me@jodyleonard.com>
2015-10-15 22:22:29 -0400

    Check collisions with integer coordinates.
    Theoretically better.

--- game.js
+++ game.js
@@ -191,7 +191,8 @@ function makeMonsterImage()
 
 //Check if obj1 is colliding with obj2//
 function checkCollision(obj1, obj2) {
-	if(obj1.y < obj2.y+obj2.height && obj1.y+obj1.height > obj2.y && obj1.x+obj1.width > obj2.x && obj1.x < obj2.x+obj2.width) {
+	//Using ~~ to truncate
+	if(~~obj1.y < ~~obj2.y + ~~obj2.height && ~~obj1.y + ~~obj1.height > ~~obj2.y && ~~obj1.x + ~~obj1.width > ~~obj2.x && ~~obj1.x < ~~obj2.x + ~~obj2.width) {
 		return true
 	}
 
