be292662ebec3a47af82dc49121f6076a7a9af1e
Jody Leonard <me@jodyleonard.com>
2015-10-29 17:26:32 -0400

    Separated out platform.js.

--- game.js
+++ game.js
@@ -1,16 +1,3 @@
-//Platform definitions//
-function Platform(x,y,width,height,color) {
-	this.x = x; this.y = y; this.width = width; this.height = height; this.color = color;
-	this.removeNow = false;
-}
-Platform.prototype.draw = function(){
-	context.fillStyle = this.color;
- 	context.fillRect(this.x,this.y,this.width,this.height);
-}
-Platform.prototype.update = function(){
-	this.x += platformScrollSpeed;
-}
-
 var monsterImage = new Image();
 function makeMonsterImage()
 {
---
+++ platform.js
@@ -0,0 +1,12 @@
+//Platform definitions//
+function Platform(x,y,width,height,color) {
+	this.x = x; this.y = y; this.width = width; this.height = height; this.color = color;
+	this.removeNow = false;
+}
+Platform.prototype.draw = function(){
+	context.fillStyle = this.color;
+ 	context.fillRect(this.x,this.y,this.width,this.height);
+}
+Platform.prototype.update = function(){
+	this.x += platformScrollSpeed;
+}
