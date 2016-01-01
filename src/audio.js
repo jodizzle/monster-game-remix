bc3dbf8c420acad4d4efa0472dca819e52dbb347
Jody Leonard <me@jodyleonard.com>
2015-12-31 23:30:46 -0500

    Add method to change music
    When loading the game, a random audio track is chosen (as before), and
    then the player can press 'c' to cycle through a list of all songs.

--- src/audio.js
+++ src/audio.js
@@ -4,23 +4,38 @@ myAudioList = ['assets/bu-an-ogres-knight-fixed.ogg',
 			   'assets/bu-crazed-and-poor-fixed.ogg',
 			   'assets/bu-a-bananas-reports-fixed.ogg'];
 //Choose a random song
-myAudio = new Audio(myAudioList[Math.floor(Math.random()*myAudioList.length)]);
+audioIndex = Math.floor(Math.random()*myAudioList.length);
+myAudio = new Audio(myAudioList[audioIndex]);
 myAudio.volume = 0.3;
+
+function changeSong() {
+	audioIndex += 1;
+	if(audioIndex >= myAudioList.length) {
+		audioIndex = 0;
+	}
+	myAudio.pause();
+	myAudio.src = myAudioList[audioIndex];
+	myAudio.play();
+}
+
 myAudio.addEventListener('ended', function() {
     this.currentTime = 0;
     this.play();
 }, false);
 
-//Keyboard listener for 'p' key//
+//Keyboard listeners//
 window.addEventListener('keydown', function(e) {
 	switch (e.which) {
-		case 80:
+		case 80: //'p'
 			if(!myAudio.paused) {
 				myAudio.pause();
 			}
 			else {
 				myAudio.play();
 			}
+			break;
+		case 67: //'c'
+			changeSong();
 			break;
 	}
 });
